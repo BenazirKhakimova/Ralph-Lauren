ebad96a5f4224608c5ff53e72e79c90cf2025383
benazirkhakimova <khakimovabenazir91@gmail.com>
2022-03-30 19:56:10 +0600

    add routing

---
+++ src/Routing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminPage from "./page/AdminPage";
+
+const Routing = () => {
+  let PUBLIC_ROUTES = {};
+
+  let ADMIN_ROUTES = {
+    link: "/admin",
+    element: <AdminPage />,
+    id: 1,
+  };
+
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route key={item.id} path={item.link} element={item.element} />
+        ))}
+        {ADMIN_ROUTES.map((item) => (
+          <Route key={item.id} path={item.link} element={item.element} />
+        ))}
+        {/* <Route path="*" element={<Error404 />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
