f089a303c4e9d38098dcaf3a2afff00fe97c1c5f
benazir <benazirkhakimova91@gmail.com>
2022-04-06 16:32:50 +0600

    finish cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -17,7 +17,14 @@ const Cart = () => {
         footer={<h2>Total: ${cart.totalPrice}</h2>}
         renderItem={(item) => (
           <List.Item
-            extra={<img src={item.item.image1} alt="img" width={272} />}
+            extra={
+              <img
+                src={item.item.image1}
+                alt="img"
+                width={272}
+                className="cart-img"
+              />
+            }
           >
             <List.Item.Meta
               title={
@@ -30,14 +37,17 @@ const Cart = () => {
                 >
                   <div>
                     <h3>{item.item.brand}</h3>
-                    <h4>{item.item.title}</h4>
+                    <h2>{item.item.title}</h2>
                   </div>
                   <h3>{"$" + item.item.price}</h3>
                 </div>
               }
               description={
                 <>
-                  <div>{item.item.description}</div>
+                  <div>
+                    {" "}
+                    <h4>{item.item.description}</h4>
+                  </div>
                   <div
                     style={{
                       display: "flex",
@@ -49,7 +59,7 @@ const Cart = () => {
                     <div>
                       <h4>Quantity</h4>
                       <Button
-                        className="btn-art"
+                        className="btn-cart"
                         onClick={() =>
                           changeProductCount(item.count - 1, item.item.id)
                         }
@@ -58,7 +68,7 @@ const Cart = () => {
                       </Button>
                       <InputNumber disabled value={item.count} />
                       <Button
-                        className="btn-art"
+                        className="btn-cart"
                         onClick={() =>
                           changeProductCount(item.count + 1, item.item.id)
                         }
@@ -72,12 +82,12 @@ const Cart = () => {
                     </div>
                   </div>
                   <Button
-                    className="btn-art"
+                    className="btn-delete"
                     onClick={() => deleteFromCart(item.item.id)}
                   >
                     Remove from cart
                   </Button>
-                  <Button className="btn-art">Buy now</Button>
+                  <Button className="btn-buy-now">Buy now</Button>
                 </>
               }
             />
