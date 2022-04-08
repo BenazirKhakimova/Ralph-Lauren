976120454dd35d8384cac42965ab0c682faf75d7
benazir <benazirkhakimova91@gmail.com>
2022-04-08 12:57:27 +0600

    deploy again

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,4 +1,4 @@
-export const PRODUCTS_API = "http://localhost:8000/products";
+export const PRODUCTS_API = "https://ralph-lauren-js.herokuapp.com/products";
 
 export const ADMIN_EMAIL = "polo@gmail.com";
 // password Lauren111
